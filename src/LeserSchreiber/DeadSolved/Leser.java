009cc7de446c9e7be59de53413b17e1f4c0cb919
Cedle <cedricjr46@gmail.com>
2020-05-02 20:02:07 +0200

    Vielleicht DeadLock/ Starvation solved4

--- src/LeserSchreiber/DeadSolved/Leser.java
+++ src/LeserSchreiber/DeadSolved/Leser.java
@@ -10,7 +10,9 @@ public class Leser extends Thread {
     private void lesen() throws InterruptedException {
         this.sleep(10);
         //DeadSolved.mut_arbeite.acquire();
+
         System.out.println("Leser"+this.id+" will lesen");
+        DeadSolved.sem_Leser.acquire();
         DeadSolved.mut_rc.acquire();
         DeadSolved.read_count++;
         if(DeadSolved.read_count == 1){
@@ -18,18 +20,19 @@ public class Leser extends Thread {
         }
 
         DeadSolved.mut_rc.release();
-        DeadSolved.sem_Leser.acquire();
+        DeadSolved.sem_Leser.release();
         Datei d = DeadSolved.DS.getDatei("/root/users/user1/desktop/datei1");
         System.out.println("Leser"+this.id+" liest: "+d.read());
-        //this.sleep(1000);
+        this.sleep(100);
+        System.out.println("Leser"+id+" ist fertig mim lesen");
         DeadSolved.mut_rc.acquire();
         DeadSolved.read_count--;
         if(DeadSolved.read_count == 0){
             DeadSolved.sem_Schreiber.release();
         }
         DeadSolved.mut_rc.release();
-        DeadSolved.sem_Leser.release();
-        System.out.println("Leser"+id+" ist fertig mim lesen");
+
+
         //DeadSolved.mut_arbeite.release();
     }
     @Override
--- src/LeserSchreiber/DeadSolved/Schreiber.java
+++ src/LeserSchreiber/DeadSolved/Schreiber.java
@@ -23,24 +23,22 @@ public class Schreiber extends Thread{
         System.out.println("Schreiber"+this.id+" will schreiben");
         //DeadSolved.mut_arbeite.acquire();
 
+
+
+
+
+        DeadSolved.sem_Leser.acquire();
+
         DeadSolved.sem_Schreiber.acquire();
-        DeadSolved.mut_wc.acquire();
-        DeadSolved.write_counter++;
-        if(DeadSolved.write_counter == 1){
-            DeadSolved.sem_Leser.acquire();
-        }
-        DeadSolved.mut_wc.release();
+        System.out.println("Schreiber"+id+" beginnt schreiben");
+        this.sleep(100);
         Datei d = DeadSolved.DS.getDatei("/root/users/user1/desktop/datei1");
         System.out.println("Schreiber"+this.id+" schreibt:"+ DeadSolved.count);
         d.write(DeadSolved.count);
         DeadSolved.count++;
-        DeadSolved.mut_wc.acquire();
-        DeadSolved.write_counter--;
-        if (DeadSolved.write_counter == 0){
-            DeadSolved.sem_Leser.release();
-        }
-        DeadSolved.mut_wc.release();
         DeadSolved.sem_Schreiber.release();
+        DeadSolved.sem_Leser.release();
+
         //DeadSolved.mut_arbeite.release();
     }
 }
