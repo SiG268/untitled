ffde4d02a18f1ad87ae137c48f882b22663aecdd
Cedle <cedricjr46@gmail.com>
2020-05-08 17:11:35 +0200

    Schreiber anpassung

--- src/LeserSchreiber/StarvationSolved/Schreiber.java
+++ src/LeserSchreiber/StarvationSolved/Schreiber.java
@@ -1,5 +1,7 @@
 package LeserSchreiber.StarvationSolved;
 
+
+
 public class Schreiber extends Thread{
     int id;
     public Schreiber(int i) {
@@ -10,29 +12,19 @@ public class Schreiber extends Thread{
     public void run() {
         while (true) {
             try {
-
+                StarvationSolved.mut_queue.acquire();
                 //Setze Sperre auf Storage
-                System.out.println("Schreiber"+id+" will schreiben");
-                StarvationSolved.mut_writeTakt.acquire();
-                StarvationSolved.mut_writeCount.acquire();
-                StarvationSolved.writeCount++;
-                if(StarvationSolved.writeCount == 1) StarvationSolved.mut_read.release();
-                StarvationSolved.mut_writeCount.release();
-                StarvationSolved.mut_write.acquire();
-
+                StarvationSolved.mut_writeStorage.acquire();
+                StarvationSolved.mut_queue.release();
                 //Schreibe
-                StarvationSolved.sharedStorage++;
+                StarvationSolved.sharedStorage ++;
                 int n = StarvationSolved.sharedStorage;
                 System.out.println("Schreiber"+id+" schreibt:" + n);
                 sleep((int)(Math.random()*1000));
-                System.out.println("Schreiber"+id+" hat geschreiben");
+
                 //Löse Sperre auf Storage
-                StarvationSolved.mut_write.release();
-                StarvationSolved.mut_writeCount.acquire();
-                StarvationSolved.writeCount--;
-                if(StarvationSolved.writeCount == 0) StarvationSolved.mut_read.release();
-                StarvationSolved.mut_writeCount.release();
-                StarvationSolved.mut_writeTakt.release();
+                StarvationSolved.mut_writeStorage.release();
+
 
             } catch (Exception e) {
                 e.printStackTrace();
--- src/LeserSchreiber/StarvationSolved/StarvationSolved.java
+++ src/LeserSchreiber/StarvationSolved/StarvationSolved.java
@@ -4,34 +4,22 @@ import java.util.concurrent.Semaphore;
 
 public class StarvationSolved {
 
+
     public static int sharedStorage = 0;
     public static int readCount = 0;
-    public static int writeCount = 0;
-    public static boolean mode = false;
-
-    public static Semaphore mut_writeTakt = new Semaphore(0);
-    public static Semaphore mut_readTakt = new Semaphore(0);
 
     public static Semaphore mut_writeStorage = new Semaphore(1);
     public static Semaphore mut_readCount = new Semaphore(1);
-    public static Semaphore mut_read = new Semaphore(1,true);
-    public static Semaphore mut_write = new Semaphore(1,true);
-    public static Semaphore mut_wantread = new Semaphore(1);
-    public static Semaphore mut_writeCount = new Semaphore(1);
-    public static Semaphore mut_wantReadCount = new Semaphore(1);
-    public static int wantReadCount = 0;
-    public static Semaphore mut_wantWriteCount = new Semaphore(1);
-    public static int wantWriteCount = 0;
+    public static Semaphore mut_queue = new Semaphore(1,true);
 
 
     public static void main(String[] args) {
-        Takt t = new Takt();
+
         Leser l1 = new Leser(1);
         Leser l2 = new Leser(2);
         Leser l3 = new Leser(3);
         Schreiber s1 = new Schreiber(1);
         Schreiber s2 = new Schreiber(2);
-        t.start();
         s1.start();
         s2.start();
         l3.start();
